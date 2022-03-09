dd8754b34fd7863449912d253eb37ec687e7054e
Sittitep Tosuwan <sittiteptosuwan@Sittiteps-MacBook-Pro.local>
2022-03-09 23:25:27 +0700

    feat: add text editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import CodeMirror from "codemirror"
+import "codemirror/mode/ruby/ruby.js"
+import "codemirror/lib/codemirror.css"
+// import "codemirror.css"
+// import "codemirror/mode"
+
+const codeEditor = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
+  lineNumbers: true,
+  mode: "ruby",
+  tabSize: 2,
+});
+
+const codeEditorWrapper = codeEditor.display.wrapper
+codeEditorWrapper.classList.add("border", "rounded-md", "mb-4");
